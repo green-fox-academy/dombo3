07f422d5da5067447b197572957be1a74ed9af97
Dombovari Peter <dombo.peter@gmail.com>
2017-03-21 13:14:13 +0100

    try out Java variables

--- week-02/day-02/HelloWorld/src/HelloWorld.java
+++ week-02/day-02/HelloWorld/src/HelloWorld.java
@@ -3,6 +3,17 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld, \n \"hello\"!");
+        System.out.println(0.3);
+        System.out.println(true);
+        int apples = 123;
+        System.out.println(apples);
+        boolean isMarried = true;
+        double pi = 3.14;
+        short peaches = 1;
+        String text = "majom";
+        byte b = 127;
+        System.out.println(++peaches);
+        System.out.println(peaches);
     }
 }
