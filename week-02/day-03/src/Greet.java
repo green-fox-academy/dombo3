59315d3146ecdd5fc7fea2fae75b478c3f190163
Dombovari Peter <dombo.peter@gmail.com>
2017-03-22 13:57:53 +0100

    finished Greet exercise

---
+++ week-02/day-03/src/Greet.java
@@ -0,0 +1,13 @@
+/**
+ * Created by user on 2017.03.22..
+ */
+public class Greet {
+  public static void main(String[] args) {
+    String a1 = "Greenfox";
+    greet(a1);
+  }
+
+  public static void greet(String name) {
+    System.out.println("Greetings dear, " + name + "!");
+  }
+}
