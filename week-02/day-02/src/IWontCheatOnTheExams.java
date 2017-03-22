66dc46a90925191888b1276df7362836f8c7f165
Dombovari Peter <dombo.peter@gmail.com>
2017-03-22 12:17:22 +0100

    finished exercise IWontCheatOnTheExam

---
+++ week-02/day-02/src/IWontCheatOnTheExams.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 2017.03.22..
+ */
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I won't cheat on the exam!");
+    }
+  }
+}
