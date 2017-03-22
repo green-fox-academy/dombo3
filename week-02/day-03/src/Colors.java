09d7cb5b2e97dded9f7322db4dd605f2531766ba
Dombovari Peter <dombo.peter@gmail.com>
2017-03-22 17:14:19 +0100

    add main function

--- week-02/day-03/src/Colors.java
+++ week-02/day-03/src/Colors.java
@@ -2,9 +2,11 @@
  * Created by user on 2017.03.22..
  */
 public class Colors {
-  String[][] colors = {
-          {"lime", "forest green", "olive", "pale green", "spring green"},
-          {"orange red", "red", "tomato"},
-          {"orchid", "violet", "pink", "hot pink"}
+  public static void main(String[] args) {
+    String[][] colors = {
+            {"lime", "forest green", "olive", "pale green", "spring green"},
+            {"orange red", "red", "tomato"},
+            {"orchid", "violet", "pink", "hot pink"}
+    };
   }
 }
