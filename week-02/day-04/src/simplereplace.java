5ef609985ac0d5fda4f61f57d02e2a026e5f1d15
Dombovari Peter <dombo.peter@gmail.com>
2017-03-23 11:59:06 +0100

    add example variable, 7th line

---
+++ week-02/day-04/src/simplereplace.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 2017.03.23..
+ */
+public class simplereplace {
+  public static void main(String[] args) {
+    String example = "In a dishwasher far far away";
+    example = example.replace("dishwasher", "galaxy");
+    
+    System.out.println(example);
+  }
+}
