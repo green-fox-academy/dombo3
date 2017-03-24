35d1d6e33933141e20c95b2a9e46b142388ed7c6
Dombovari Peter <dombo.peter@gmail.com>
2017-03-24 11:21:08 +0100

    Palindrome method skeleton ready

---
+++ week-02/day-04/src/CreatePalindrome.java
@@ -0,0 +1,13 @@
+/**
+ * Created by user on 2017.03.24..
+ */
+public class CreatePalindrome {
+  public static void main(String[] args) {
+    System.out.println(Palindrome("Palindrome ready"));
+  }
+
+  public static String Palindrome(String word) {
+
+    return word;
+  }
+}
