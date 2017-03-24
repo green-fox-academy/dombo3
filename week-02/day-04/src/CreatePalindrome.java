f1971d35b4412bfdad0921ebdd06767487c663cc
Dombovari Peter <dombo.peter@gmail.com>
2017-03-24 11:52:19 +0100

    feat(main): ask user for input with scanner

--- week-02/day-04/src/CreatePalindrome.java
+++ week-02/day-04/src/CreatePalindrome.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 /**
  * Created by user on 2017.03.24..
  */
 public class CreatePalindrome {
   public static void main(String[] args) {
+    Scanner myscanner = new Scanner(System.in);
+    System.out.println("Palondrinomiser, type in a word please: ");
+    String word = myscanner.nextLine();
 
-    System.out.println(Palindrome("Palindrome ready"));
+    System.out.println(Palindrome(word));
   }
 
   public static String Palindrome(String word) {
