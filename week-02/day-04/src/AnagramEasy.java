4d18f3a4c2bc6bfc30575a90cf920384dd189394
Dombovari Peter <dombo.peter@gmail.com>
2017-03-24 10:41:16 +0100

    add is Anagram method and Scanner to main

---
+++ week-02/day-04/src/AnagramEasy.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by user on 2017.03.24..
+ */
+public class AnagramEasy {
+  public static void main(String[] args) {
+    Scanner myscanner = new Scanner(System.in);
+    System.out.println("Plese type in your first word: ");
+    String word1 = myscanner.nextLine();
+    System.out.println("Plese type in your second word: ");
+    String word2 = myscanner.nextLine();
+
+    if (IsAnagram(word1, word2)) {
+      System.out.println("It is an anagram");
+    } else {
+      System.out.println("It is not an anagram");
+    }
+  }
+
+  public static boolean IsAnagram(String word1, String word2) {
+
+    char[] word1Char = word1.toCharArray();
+    char[] word2Char = word2.toCharArray();
+
+    Arrays.sort(word1Char);
+    Arrays.sort(word2Char);
+
+    return Arrays.equals(word1Char, word2Char);
+  }
+}
