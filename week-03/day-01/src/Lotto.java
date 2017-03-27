213125d60b7af1435de25ce5e07c6b0f9675a118
Dombovari Peter <dombo.peter@gmail.com>
2017-03-27 11:17:00 +0200

    feat(substring): get the last 5 number from the list as substring

--- week-03/day-01/src/Lotto.java
+++ week-03/day-01/src/Lotto.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,8 +15,21 @@ public class Lotto {
       Path filePath = Paths.get("assets/otos.csv");
       List<String> lines = Files.readAllLines(filePath);
       for (String line : lines) {
-        System.out.println(line.toUpperCase());
+        line.substring(line.lastIndexOf("Ft") + 3);
       }
+
+      ArrayList<String> otos = new ArrayList<String>();
+
+      for (int i = 0; i < lines.size(); i++) {
+        String hi = lines.get(i).substring(lines.get(i).lastIndexOf("Ft") + 3);
+        otos.add(hi);
+      }
+      
+
+      System.out.println(otos);
+
+
+
     } catch (Exception e) {
       System.out.println("Error happened: " + e.getClass());
     }
