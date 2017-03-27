471b91bd36666028766909f0d4961e8c7a460786
Dombovari Peter <dombo.peter@gmail.com>
2017-03-27 21:26:59 +0200

    feat(decrypt): decrypt lines add to list

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -1,20 +1,35 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by user on 2017.03.27..
- */
 public class Doubled {
+  final public static String TXT_PATH = "assets/duplicated-chars.txt";
   public static void main(String[] args) {
-    try {
-      Path path = Paths.get("assets/duplicated-chars.txt");
-      List<String> lines = Files.readAllLines(path);
-      System.out.println(lines);
+    List<String> lines = readFileLines();
 
+    List<StringBuilder> decrypLines = new ArrayList<>();
+    for (int i = 0; i < lines.size(); i++) {
+      StringBuilder sb = new StringBuilder();
+      for (int j = 0; j < lines.get(i).length(); j += 2) {
+        sb.append(lines.get(i).charAt(j));
+      }
+      decrypLines.add(sb);
+    }
+    System.out.println(decrypLines);
+  }
+
+  public static List<String> readFileLines() {
+    List<String> lines;
+    try {
+      Path path = Paths.get(TXT_PATH);
+      lines = Files.readAllLines(path);
     } catch (Exception e){
       System.out.println("Exception occured: " + e.getClass());
+      lines = new ArrayList<>();
     }
+    System.out.println("File is loaded");
+    return lines;
   }
 }
