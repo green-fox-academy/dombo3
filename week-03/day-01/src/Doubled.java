3333a2359a2993501367c27dd4feee826d7002fa
Dombovari Peter <dombo.peter@gmail.com>
2017-03-28 08:00:07 +0200

    derypter method decrypts text and file is written out in txt

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -6,18 +7,35 @@ import java.util.List;
 
 public class Doubled {
   final public static String TXT_PATH = "assets/duplicated-chars.txt";
+  final public static String DECRYPT_TXT = "assets/decrypted-chars.txt";
+
   public static void main(String[] args) {
     List<String> lines = readFileLines();
+    List<StringBuilder> decryptedText = decrypter(lines);
+    writeFileLines(decryptedText);
+  }
 
-    List<StringBuilder> decrypLines = new ArrayList<>();
-    for (int i = 0; i < lines.size(); i++) {
-      StringBuilder sb = new StringBuilder();
-      for (int j = 0; j < lines.get(i).length(); j += 2) {
-        sb.append(lines.get(i).charAt(j));
+  public static List<StringBuilder> decrypter(List<String> lines) {
+      List<StringBuilder> decryptLines = new ArrayList<>();
+      for (int i = 0; i < lines.size(); i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < lines.get(i).length(); j += 2) {
+          sb.append(lines.get(i).charAt(j));
+        }
+        decryptLines.add(sb);
       }
-      decrypLines.add(sb);
+      return decryptLines;
     }
-    System.out.println(decrypLines);
+
+  public static void writeFileLines(List<StringBuilder> decryptedText) {
+    try {
+      Path path = Paths.get(DECRYPT_TXT);
+      Files.write(path, decryptedText);
+      System.out.println("Mission accomplished. The TEXT is decrypted...");
+      System.out.println("Check file at: " + DECRYPT_TXT);
+    } catch (IOException e) {
+      System.out.println("Exception occured: " + e.getClass());
+    }
   }
 
   public static List<String> readFileLines() {
@@ -25,7 +43,7 @@ public class Doubled {
     try {
       Path path = Paths.get(TXT_PATH);
       lines = Files.readAllLines(path);
-    } catch (Exception e){
+    } catch (IOException e){
       System.out.println("Exception occured: " + e.getClass());
       lines = new ArrayList<>();
     }
