3ec053fd0879e44f09137c9cd3fa4a3389441dab
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 15:29:29 +0200

    add boilerplate for drawing

---
+++ week-03/day-03/src/Checkerboard.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+
+  public static final int WIDTH = 300;
+  public static final int HEIGHT = 300;
+
+  public static void mainDraw(Graphics graphics) {
+
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
