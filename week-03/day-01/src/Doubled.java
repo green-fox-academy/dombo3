2d85262d0f161c058e710d42e7f41cd2a7a4162a
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 09:53:42 +0200

    feat(UX): print hacking style lines to console

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -12,20 +12,30 @@ public class Doubled {
   public static void main(String[] args) {
     List<String> lines = readFileLines();
     List<StringBuilder> decryptedText = decrypter(lines);
+    String message = "Start hacking";
+    String dots = "...";
+    String matrix =
+            "\n100010011110000000010101011111100010101101100010001001000111" +
+            "\n010001001000010001001111111110010000101100101000000111101100" +
+            "\n010101101100100101101011010101010101011010101010110000011111" +
+            "\n111111001010100001110101000101000110101011100110101011100010\n";
+    slowPrint(message, 400);
+    slowPrint(dots, 500);
+    slowPrint(matrix, 50);
     writeFileLines(decryptedText);
   }
 
   public static List<StringBuilder> decrypter(List<String> lines) {
-      List<StringBuilder> decryptLines = new ArrayList<>();
-      for (int i = 0; i < lines.size(); i++) {
-        StringBuilder sb = new StringBuilder();
-        for (int j = 0; j < lines.get(i).length(); j += 2) {
-          sb.append(lines.get(i).charAt(j));
-        }
-        decryptLines.add(sb);
+    List<StringBuilder> decryptLines = new ArrayList<>();
+    for (int i = 0; i < lines.size(); i++) {
+      StringBuilder sb = new StringBuilder();
+      for (int j = 0; j < lines.get(i).length(); j += 2) {
+        sb.append(lines.get(i).charAt(j));
       }
-      return decryptLines;
+      decryptLines.add(sb);
     }
+    return decryptLines;
+  }
 
   public static void writeFileLines(List<StringBuilder> decryptedText) {
     try {
@@ -43,11 +53,22 @@ public class Doubled {
     try {
       Path path = Paths.get(TXT_PATH);
       lines = Files.readAllLines(path);
-    } catch (IOException e){
+    } catch (IOException e) {
       System.out.println("Exception occured: " + e.getClass());
       lines = new ArrayList<>();
     }
     System.out.println("File is loaded");
     return lines;
+  }
+
+  public static void slowPrint(String message, long millisPerChar) {
+    for (int i = 0; i < message.length(); i++) {
+      System.out.print(message.charAt(i));
+      try {
+        Thread.sleep(millisPerChar);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
