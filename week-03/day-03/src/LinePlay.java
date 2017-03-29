c4cddb2cdfbc05220c791e766880c6181a29eb17
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 14:18:20 +0200

    feat(draw): draw lines to get weird result

---
+++ week-03/day-03/src/LinePlay.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+
+  public static final int WIDTH = 300;
+  public static final int HEIGHT = 300;
+
+  public static void mainDraw(Graphics graphics) {
+    for (int i = 20; i < 300; i += 10){
+      graphics.drawLine(0, i, i,300);
+    }
+    for (int i = 20; i < 300; i += 10){
+      graphics.drawLine(i, 0, 300,i);
+    }
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
