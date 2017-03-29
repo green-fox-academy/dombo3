4ba3dc1c2e18d2009cf559162743e427e909dd03
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 11:53:21 +0200

    add boilerplate code for drawing

---
+++ week-03/day-03/src/PositionSquare.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+  public static final int WIDTH = 300;
+  public static final int HEIGHT = 300;
+
+  public static void mainDraw(Graphics graphics) {
+
+  }
+
+  public static void myFunction(Graphics graphics) {
+
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
