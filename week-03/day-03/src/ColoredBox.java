dcac0aed81d3a6163492515f9503581d265d54a5
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 10:34:34 +0200

    feat(draw): draw rectangle

--- week-03/day-03/src/ColoredBox.java
+++ week-03/day-03/src/ColoredBox.java
@@ -1,17 +1,29 @@
 import javax.swing.*;
-
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class ColoredBox {
 
-  public static void mainDraw(Graphics graphics){
+  public static void mainDraw(Graphics graphics) {
     // draw a box that has different colored lines on each edge.
+    int startPointX = 20;
+    int startPointY = 20;
+    int width = 80;
+    int height = 80;
 
+    graphics.setColor(Color.BLUE);
+    graphics.drawLine(startPointX, startPointY, startPointX, startPointY + height);
+    graphics.setColor(Color.GREEN);
+    graphics.drawLine(startPointX, startPointY + height, startPointX + width, startPointY +
+              height);
+    graphics.setColor(Color.YELLOW);
+    graphics.drawLine(startPointX + width, startPointY + height, startPointX + width,
+              startPointY);
+    graphics.setColor(Color.RED);
+    graphics.drawLine(startPointX + width, startPointY, startPointX, startPointY);
   }
 
-  //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(300, 300));
@@ -20,6 +32,7 @@ public class ColoredBox {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
+
   static class ImagePanel extends JPanel{
     @Override
     protected void paintComponent(Graphics graphics) {
