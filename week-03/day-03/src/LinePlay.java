6398fbf92ccfea32db6c973198af877347601191
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 14:23:14 +0200

    chore(weirdlines): set color and size of weirdlines pattern as shown

--- week-03/day-03/src/LinePlay.java
+++ week-03/day-03/src/LinePlay.java
@@ -9,10 +9,12 @@ public class LinePlay {
   public static final int HEIGHT = 300;
 
   public static void mainDraw(Graphics graphics) {
-    for (int i = 20; i < 300; i += 10){
+    for (int i = 30; i < 300; i += 30){
+      graphics.setColor(Color.GREEN);
       graphics.drawLine(0, i, i,300);
     }
-    for (int i = 20; i < 300; i += 10){
+    for (int i = 30; i < 300; i += 30){
+      graphics.setColor(new Color(150,0,205));
       graphics.drawLine(i, 0, 300,i);
     }
   }
