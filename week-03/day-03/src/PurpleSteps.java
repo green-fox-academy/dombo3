1bcc78c3021b083b0724ae79426969372a316119
Dombovari Peter <dombo.peter@gmail.com>
2017-03-29 15:10:00 +0200

    feat(draw): draw rectangle with thick outline

--- week-03/day-03/src/PurpleSteps.java
+++ week-03/day-03/src/PurpleSteps.java
@@ -3,12 +3,18 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class BoilerPlate {
+public class PurpleSteps {
 
   public static final int WIDTH = 300;
   public static final int HEIGHT = 300;
 
   public static void mainDraw(Graphics graphics) {
+    Graphics2D g2d = (Graphics2D) graphics;
+    g2d.setColor(Color.RED);
+    g2d.fillRect(20,20,50,50);
+    g2d.setColor(Color.BLACK);
+    g2d.setStroke(new BasicStroke(2));
+    g2d.drawRect(20,20,50,50);
 
   }
 
@@ -30,7 +36,6 @@ public class BoilerPlate {
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
       mainDraw(graphics);
-
     }
   }
 }
