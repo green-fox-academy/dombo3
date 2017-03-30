c76ffac24184373b4f968179f99b49a7c12aefe5
Dombovari Peter <dombo.peter@gmail.com>
2017-03-30 22:51:55 +0200

    draw cross pattern recursively

--- week-03/day-04/src/GraphicOne.java
+++ week-03/day-04/src/GraphicOne.java
@@ -10,23 +10,25 @@ public class GraphicOne {
   public static final int HEIGHT = 300;
 
   public static void mainDraw(Graphics graphics) {
-    drawRectCenter(300, graphics);
-    drawRect(graphics);
+    drawPattern(0,0, 300, graphics);
   }
 
-  public static void drawRect(Graphics graphics) {
-    graphics.setColor(Color.BLACK);
-    int size = HEIGHT/3;
-    graphics.drawRect(WIDTH/3,0, size, size);
-    graphics.drawRect(2*WIDTH/3,HEIGHT/3, size, size);
-    graphics.drawRect(WIDTH/3,2*HEIGHT/3, size, size);
-    graphics.drawRect(0,HEIGHT/3, size, size);
+  public static void drawPattern(int posx, int posy, int size, Graphics graphics) {
+    if (size < 1) {
+      return;
+    } else {
+      graphics.setColor(Color.BLACK);
+      graphics.drawRect(posx, posy, size, size);
+      drawPattern(posx + size/3, posy, size/3, graphics);
+      drawPattern(posx + 2*size/3,posy + size/3, size/3, graphics);
+      drawPattern(posx + size/3,posy + 2 * size/3, size/3, graphics);
+      drawPattern(posx,posy + size/3, size/3, graphics);
+//      times--;
+    }
   }
 
+  public static void drawRectangle(int posx, int posy, int size, Graphics graphics) {
 
-  public static void drawRectCenter(int size, Graphics graphics) {
-    graphics.setColor(Color.BLACK);
-    graphics.drawRect(WIDTH/2 - size/2,HEIGHT/2 - size/2, size, size);
   }
 
   public static void main(String[] args) {
