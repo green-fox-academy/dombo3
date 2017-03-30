59ab3adcbce6c743c830d38cb87a7176e06c08f8
Dombovari Peter <dombo.peter@gmail.com>
2017-03-30 15:37:24 +0200

    draw basic pattern for recursive draw

--- week-03/day-04/src/GraphicOne.java
+++ week-03/day-04/src/GraphicOne.java
@@ -10,11 +10,23 @@ public class GraphicOne {
   public static final int HEIGHT = 300;
 
   public static void mainDraw(Graphics graphics) {
-
+    drawRectCenter(300, graphics);
+    drawRect(graphics);
   }
 
-  public static void drawCross(Graphics graphics) {
+  public static void drawRect(Graphics graphics) {
+    graphics.setColor(Color.BLACK);
+    int size = HEIGHT/3;
+    graphics.drawRect(WIDTH/3,0, size, size);
+    graphics.drawRect(2*WIDTH/3,HEIGHT/3, size, size);
+    graphics.drawRect(WIDTH/3,2*HEIGHT/3, size, size);
+    graphics.drawRect(0,HEIGHT/3, size, size);
+  }
 
+
+  public static void drawRectCenter(int size, Graphics graphics) {
+    graphics.setColor(Color.BLACK);
+    graphics.drawRect(WIDTH/2 - size/2,HEIGHT/2 - size/2, size, size);
   }
 
   public static void main(String[] args) {
@@ -30,8 +42,8 @@ public class GraphicOne {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
+      this.setBackground(Color.YELLOW);
       mainDraw(graphics);
-
     }
   }
 }
