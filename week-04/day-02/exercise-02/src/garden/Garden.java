eb7f0b85de547c126a25fdfcd9b3a013baeca7e2
Dombovari Peter <dombo.peter@gmail.com>
2017-04-04 14:14:29 +0200

    feat(Garden): check water amount of plants

--- week-04/day-02/exercise-02/src/garden/Garden.java
+++ week-04/day-02/exercise-02/src/garden/Garden.java
@@ -20,4 +20,36 @@ public class Garden {
   public void addTree(Tree tree) {
     trees.add(tree);
   }
+
+  public int checkFlowers() {
+    int flowerNeedWater = 0;
+    for (int i = 0; i < flowers.size(); i++) {
+      if (flowers.get(i).currentWater < 5) {
+        flowerNeedWater += 1;
+      }
+    }
+    return flowerNeedWater;
+  }
+
+  public int checkTrees() {
+    int treeNeedWater = 0;
+    for (int i = 0; i < flowers.size(); i++) {
+      if (flowers.get(i).currentWater < 10) {
+        treeNeedWater += 1;
+      }
+    }
+    return treeNeedWater;
+  }
+
+
+  public int irrigate(int waterAmount){
+    int plantNeedWater = checkFlowers() + checkTrees();
+    if (plantNeedWater == 0) {
+      System.out.println("No plant needs water!");
+    } else {
+      waterAmount = waterAmount/plantNeedWater;
+    }
+
+    return waterAmount;
+  }
 }
