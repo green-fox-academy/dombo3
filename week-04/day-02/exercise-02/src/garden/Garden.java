0c8284da64e51ab35e0b48d94391995ccdf7b66b
Dombovari Peter <dombo.peter@gmail.com>
2017-04-04 13:48:26 +0200

    feat(Garden): create constructor and add methods

--- week-04/day-02/exercise-02/src/garden/Garden.java
+++ week-04/day-02/exercise-02/src/garden/Garden.java
@@ -1,7 +1,23 @@
 package garden;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garden {
-  public void check() {
-    System.out.println("I am in the Garden!");
+
+  private List<Flower> flowers;
+  private List<Tree> trees;
+
+  public Garden(){
+    flowers = new ArrayList<>();
+    trees = new ArrayList<>();
+  }
+
+  public void addFlower(Flower flower) {
+    flowers.add(flower);
+  }
+
+  public void addTree(Tree tree) {
+    trees.add(tree);
   }
 }
