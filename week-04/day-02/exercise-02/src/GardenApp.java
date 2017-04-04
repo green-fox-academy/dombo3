de5e27c892b993e34d20331f988c9f12b7e45ebb
Dombovari Peter <dombo.peter@gmail.com>
2017-04-04 13:50:09 +0200

    feat(GardenApp): test instances and methods

--- week-04/day-02/exercise-02/src/GardenApp.java
+++ week-04/day-02/exercise-02/src/GardenApp.java
@@ -2,7 +2,17 @@ import garden.*;
 
 public class GardenApp {
   public static void main(String[] args) {
+
     Garden myGarden = new Garden();
-    myGarden.check();
+
+    Plant yellow = new Flower(25,75);
+    Plant blue = new Flower(10,75);
+    Plant purple = new Tree(40, 40);
+    Plant orange = new Tree(30, 40);
+
+    yellow.getCurrentWater();
+    blue.getAbsorbRate();
+    purple.getCurrentWater();
+    orange.getAbsorbRate();
   }
 }
