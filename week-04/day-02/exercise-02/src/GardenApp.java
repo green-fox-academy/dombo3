7affadf45cd264d0647e6e58ff8a884e0bd1c3d0
Dombovari Peter <dombo.peter@gmail.com>
2017-04-04 12:32:18 +0200

    feat(Garden): setup tested project skeleton

---
+++ week-04/day-02/exercise-02/src/GardenApp.java
@@ -0,0 +1,8 @@
+import garden.*;
+
+public class GardenApp {
+  public static void main(String[] args) {
+    Garden myGarden = new Garden();
+    myGarden.check();
+  }
+}
---
+++ week-04/day-02/exercise-02/src/garden/Garden.java
@@ -0,0 +1,7 @@
+package garden;
+
+public class Garden {
+  public void check() {
+    System.out.println("I am in the Garden!");
+  }
+}
