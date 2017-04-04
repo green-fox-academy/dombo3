02808aef65fa646084435c7009f01bed797c47ed
Dombovari Peter <dombo.peter@gmail.com>
2017-04-04 20:28:07 +0200

    refact(All): Lvl 2, getting smarter. Refact ALL of the code, Trees and Flower List merged into Plants List... changes everything

--- week-04/day-02/exercise-02/src/garden/Garden.java
+++ week-04/day-02/exercise-02/src/garden/Garden.java
@@ -5,82 +5,129 @@ import java.util.List;
 
 public class Garden {
 
-  private List<Flower> flowers;
-  private List<Tree> trees;
+  private List<Plant> plants;
 
   public Garden(){
-    flowers = new ArrayList<>();
-    trees = new ArrayList<>();
+      plants = new ArrayList<>();
   }
 
-  public void addFlower(Flower flower) {
-    flowers.add(flower);
+  public void addPlant(Plant plant) {
+    plants.add(plant);
   }
 
-  public void addTree(Tree tree) {
-    trees.add(tree);
-  }
+  public List<Integer> checkPlants() {
+    List<Integer> plantsNeedWater = new ArrayList<>();
+    for (int i = 0; i < plants.size(); i++) {
+      if (plants.get(i).getCurrentWater() < plants.get(i).getThirstyLine());
 
-  public List<Integer> checkFlowers() {
-    List<Integer> flowersNeedsWater = new ArrayList<>();
-    for (int i = 0; i < flowers.size(); i++) {
-      if (flowers.get(i).getCurrentWater() < 5) {
-        flowersNeedsWater.add(i);
-      }
+      // add something please
+    
     }
-    return flowersNeedsWater;
+    return plantsNeedWater;
   }
 
-  public List<Integer> checkTrees() {
-    List<Integer> treesNeedsWater = new ArrayList<>();
-    for (int i = 0; i < trees.size(); i++) {
-      if (trees.get(i).getCurrentWater() < 10) {
-        treesNeedsWater.add(i);
-      }
-    }
-    return treesNeedsWater;
-  }
-
-  public int waterAmountPerPlant(int waterAmount){
-    int plantNeedWater = checkFlowers().size() + checkTrees().size();
+  public int waterAmountPerPlant(int waterAmount) {
     int waterAmountPerPlant = 0;
-    if (plantNeedWater == 0) {
+    if (checkPlants().size() == 0) {
       System.out.println("No plant needs water!");
     } else {
-      waterAmountPerPlant = waterAmount/plantNeedWater;
+      waterAmountPerPlant = waterAmount/checkPlants().size();
     }
     return waterAmountPerPlant;
   }
 
   public void irrigate(int waterAmount) {
-    for (int index : checkFlowers()) {
-      flowers.get(index).setCurrentWater(flowers.get(index).getCurrentWater() +
+    for (int index : checkPlants()) {
+      plants.get(index).setCurrentWater(plants.get(index).getCurrentWater() +
               waterAmountPerPlant(waterAmount));
     }
-    for (int index : checkTrees()) {
-      trees.get(index).setCurrentWater(trees.get(index).getCurrentWater() +
-              waterAmountPerPlant(waterAmount));
-    }
-
     System.out.println("Watering with " + waterAmount + "!");
   }
 
   public void look() {
-    for (Tree tree : trees) {
-      if (tree.getCurrentWater() < 10) {
-        System.out.println("The " + tree.getName() + " Tree does need water.");
+    for (Plant plant : plants) {
+      if (plant.getCurrentWater() < plant.getThirstyLine()) {
+        System.out.println("The " + plant.getName() + " " + plant.getClass().getSimpleName() + " does " +
+                "need" + " " + "water.");
       } else {
-        System.out.println("The " + tree.getName() + " Tree doesn't need water.");
+        System.out.println("The " + plant.getName() + " " + plant.getClass().getSimpleName() + " " +
+                "doesn't need" + " " + "water.");
       }
     }
-
-    for (Flower flower : flowers) {
-      if (flower.getCurrentWater() < 5) {
-        System.out.println("The " + flower.getName() + " Flower does need water.");
-      } else {
-        System.out.println("The " + flower.getName() + " Flower doesn't need water.");
-      }
-    }
-
   }
 }
+
+//  private List<Flower> flowers;
+//  private List<Tree> trees;
+//
+//  flowers = new ArrayList<>();
+//  trees = new ArrayList<>();
+//
+//  public void addFlower(Flower flower) {
+//    flowers.add(flower);
+//  }
+//
+//  public void addTree(Tree tree) {
+//    trees.add(tree);
+//  }
+//
+//  public List<Integer> checkFlowers() {
+//    List<Integer> flowersNeedsWater = new ArrayList<>();
+//    for (int i = 0; i < flowers.size(); i++) {
+//      if (flowers.get(i).getCurrentWater() < 5) {
+//        flowersNeedsWater.add(i);
+//      }
+//    }
+//    return flowersNeedsWater;
+//  }
+//
+//  public List<Integer> checkTrees() {
+//    List<Integer> treesNeedsWater = new ArrayList<>();
+//    for (int i = 0; i < trees.size(); i++) {
+//      if (trees.get(i).getCurrentWater() < 10) {
+//        treesNeedsWater.add(i);
+//      }
+//    }
+//    return treesNeedsWater;
+//  }
+//
+//  public int waterAmountPerPlant(int waterAmount){
+//    int plantNeedWater = checkFlowers().size() + checkTrees().size();
+//    int waterAmountPerPlant = 0;
+//    if (plantNeedWater == 0) {
+//      System.out.println("No plant needs water!");
+//    } else {
+//      waterAmountPerPlant = waterAmount/plantNeedWater;
+//    }
+//    return waterAmountPerPlant;
+//  }
+//
+//  public void irrigate(int waterAmount) {
+//    for (int index : checkFlowers()) {
+//      flowers.get(index).setCurrentWater(flowers.get(index).getCurrentWater() +
+//              waterAmountPerPlant(waterAmount);
+//    }
+//    for (int index : checkTrees()) {
+//      trees.get(index).setCurrentWater(trees.get(index).getCurrentWater() +
+//              waterAmountPerPlant(waterAmount));
+//    }
+//
+//    System.out.println("Watering with " + waterAmount + "!");
+//  }
+//
+//  public void look() {
+//    for (Tree tree : trees) {
+//      if (tree.getCurrentWater() < 10) {
+//        System.out.println("The " + tree.getName() + " Tree does need water.");
+//      } else {
+//        System.out.println("The " + tree.getName() + " Tree doesn't need water.");
+//      }
+//    }
+//
+//    for (Flower flower : flowers) {
+//      if (flower.getCurrentWater() < 5) {
+//        System.out.println("The " + flower.getName() + " Flower does need water.");
+//      } else {
+//        System.out.println("The " + flower.getName() + " Flower doesn't need water.");
+//      }
+//    }
