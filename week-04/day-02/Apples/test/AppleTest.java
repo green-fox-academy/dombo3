105972afcae32747d644adb58af37dfb481c7f9c
Dombovari Peter <dombo.peter@gmail.com>
2017-04-05 11:01:14 +0200

    create test for getApple method

--- week-04/day-02/Apples/test/AppleTest.java
+++ week-04/day-02/Apples/test/AppleTest.java
@@ -3,9 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppleTest {
+
   @Test
   public void getApple() throws Exception {
-
+    Apple apple = new Apple();
+    assertEquals("apple", apple.getApple());
   }
-
 }
