ff8d01829d108ccb5bc4f935e103c67a9147f8c2
Dombovari Peter <dombo.peter@gmail.com>
2017-04-05 12:52:19 +0200

    add Anagram class skeleton

---
+++ week-04/day-03/Anagram/src/Anagram.java
@@ -0,0 +1,16 @@
+public class Anagram {
+  private String word1;
+  private String word2;
+
+  public Anagram() {
+  }
+
+  public Anagram(String word1, String word2) {
+    this.word1 = word1;
+    this.word2 = word2;
+  }
+
+  public boolean isAnagram() {
+    return true;
+  }
+}
