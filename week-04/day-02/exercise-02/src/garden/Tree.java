ebc5f5b7df55d563ec4427b140d4c24602f077e0
Dombovari Peter <dombo.peter@gmail.com>
2017-04-05 10:21:52 +0200

    style

--- week-04/day-02/exercise-02/src/garden/Tree.java
+++ week-04/day-02/exercise-02/src/garden/Tree.java
@@ -2,7 +2,7 @@ package garden;
 
 public class Tree extends Plant {
 
-  public Tree(int currentWater, String name){
+  public Tree(int currentWater, String name) {
     super(currentWater, name, 0.75, 10);
   }
 }
