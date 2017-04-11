6d5f72a0cf277d20a268b348340fdeffea1fd149
Dombovari Peter <dombo.peter@gmail.com>
2017-04-11 12:22:26 +0200

    feat(Board): add restriction for the hero at map boundaries

--- week-05/day-02/RPGgame/src/GameObject/Board.java
+++ week-05/day-02/RPGgame/src/GameObject/Board.java
@@ -11,13 +11,17 @@ public class Board extends JComponent implements KeyListener {
   int HeroX;
   int HeroY;
   int HeroDirection;
+  final int TILESIZE = 72;
+  final int BOARDDIMENSIONX = TILESIZE * 10;
+  final int BOARDIMENSIONY = TILESIZE * 10;
 
   public Board() {
 
     HeroX = 0;
     HeroY = 0;
 
-    setPreferredSize(new Dimension(720, 720));
+
+    setPreferredSize(new Dimension(BOARDDIMENSIONX, BOARDIMENSIONY));
     setVisible(true);
   }
 
@@ -95,19 +99,26 @@ public class Board extends JComponent implements KeyListener {
   public void keyReleased(KeyEvent e) {
     // When the up or down keys hit, we change the position of our box
     if (e.getKeyCode() == KeyEvent.VK_UP) {
-      HeroY -= 72;
+      if ((HeroY - 72) >= 0) {
+        HeroY -= 72;
+      }
       HeroDirection = 0;
     } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-      HeroY += 72;
+      if ((HeroY + 72) < 720) {
+        HeroY += 72;
+      }
       HeroDirection = 2;
     } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      HeroX += 72;
+      if ((HeroX + 72) < 720) {
+        HeroX += 72;
+      }
       HeroDirection = 3;
     } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-      HeroX -= 72;
+      if ((HeroX - 72) >= 0) {
+        HeroX -= 72;
+      }
       HeroDirection = 1;
     }
-    // and redraw to have a new picture with the new coordinates
     repaint();
   }
 }
