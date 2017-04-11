1fdf82aad5ee2d6052adf26adf00db8e0184f9ce
Dombovari Peter <dombo.peter@gmail.com>
2017-04-11 14:41:26 +0200

    feat(Board): restrict area for hero, hero cannot walk trough the walls

--- week-05/day-02/RPGgame/src/GameObject/Board.java
+++ week-05/day-02/RPGgame/src/GameObject/Board.java
@@ -1,9 +1,13 @@
 package GameObject;
 
+import javafx.geometry.Pos;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Board extends JComponent implements KeyListener {
 
@@ -34,32 +38,35 @@ public class Board extends JComponent implements KeyListener {
     frame.addKeyListener(board);
   }
 
+
+  int[][] gameBoard = new int[][] {
+          { 1, 1, 1, 0, 1, 0, 1, 1, 1, 1 },
+          { 1, 1, 1, 0, 1, 0, 1, 0, 0, 1 },
+          { 1, 0, 0, 0, 1, 0, 1, 0, 0, 1 },
+          { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 },
+          { 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+          { 1, 0, 1, 0, 1, 1, 1, 1, 0, 1 },
+          { 1, 0, 1, 0, 1, 0, 0, 1, 0, 1 },
+          { 1, 1, 1, 1, 1, 0, 0, 1, 0, 1 },
+          { 1, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
+          { 1, 1, 1, 0, 1, 0, 0, 1, 1, 1 },
+  };
+
   @Override
   public void paint(Graphics graphics) {
     super.paint(graphics);
-    int[][] gameBoard = new int[][] {
-            { 1, 1, 1, 0, 1, 0, 1, 1, 1, 1 },
-            { 1, 1, 1, 0, 1, 0, 1, 0, 0, 1 },
-            { 1, 0, 0, 0, 1, 0, 1, 0, 0, 1 },
-            { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 },
-            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-            { 1, 0, 1, 0, 1, 1, 1, 1, 0, 1 },
-            { 1, 0, 1, 0, 1, 0, 0, 1, 0, 1 },
-            { 1, 1, 1, 1, 1, 0, 0, 1, 0, 1 },
-            { 1, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
-            { 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 },
-    };
 
     int row = gameBoard.length;
     int col = gameBoard[0].length;
 
+
     for (int i = 0; i < row; i++) {
       for (int j = 0; j < col; j++) {
-        if (gameBoard[i][j] == 1) {
+        if (gameBoard[j][i] == 1) {
           PositionedImage floor = new PositionedImage("assets/floor.png", i * TILESIZE, j *
                   TILESIZE);
           floor.draw(graphics);
-        } else if (gameBoard[i][j] == 0) {
+        } else if (gameBoard[j][i] == 0) {
           PositionedImage wall = new PositionedImage("assets/wall.png",i * TILESIZE, j * TILESIZE);
           wall.draw(graphics);
         }
@@ -92,29 +99,31 @@ public class Board extends JComponent implements KeyListener {
 
   }
 
+  public void getPosHero() {
+    for (int i = 0; i < 10; i++) {
+
+    }
+  }
+
+
   // But actually we can use just this one for our goals here
   @Override
   public void keyReleased(KeyEvent e) {
     // When the up or down keys hit, we change the position of our box
-    if (e.getKeyCode() == KeyEvent.VK_UP) {
-      if ((HeroY - 72) >= 0) {
-        HeroY -= 72;
-      }
+    if (e.getKeyCode() == KeyEvent.VK_UP && (HeroY - 72) >= 0 && (gameBoard[HeroY/72 - 1][HeroX/72] != 0)) {
+      HeroY -= 72;
       HeroDirection = 0;
-    } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-      if ((HeroY + 72) < 720) {
-        HeroY += 72;
-      }
+    } else if(e.getKeyCode() == KeyEvent.VK_DOWN && (HeroY + 72) < 720 && gameBoard[HeroY/72 +
+            1][HeroX/72] != 0) {
+      HeroY += 72;
       HeroDirection = 2;
-    } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      if ((HeroX + 72) < 720) {
-        HeroX += 72;
-      }
+    } else if(e.getKeyCode() == KeyEvent.VK_RIGHT && (HeroX + 72) < 720 &&
+            gameBoard[HeroY/72][HeroX/72 + 1] != 0) {
+      HeroX += 72;
       HeroDirection = 3;
-    } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-      if ((HeroX - 72) >= 0) {
-        HeroX -= 72;
-      }
+    } else if(e.getKeyCode() == KeyEvent.VK_LEFT && (HeroX - 72) >= 0 &&
+    gameBoard[HeroY/72][HeroX/72-1] != 0) {
+      HeroX -= 72;
       HeroDirection = 1;
     }
     repaint();
