23c103d95d4891457b7782999b4b54c36553f14c
Dombovari Peter <dombo.peter@gmail.com>
2017-05-02 17:49:49 +0200

    add greeting in different languages

--- week-08/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
+++ week-08/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
@@ -1,8 +1,8 @@
 package com.greenfoxacademy.controllers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,9 +20,20 @@ public class HelloWebController {
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam(value = "name", defaultValue = "World", required = false) String name) {
+    Random rand = new Random();
+    List<Integer> fonts = new ArrayList<>();
+    for (int i = 0; i < hellos.length; i++) {
+      int  n = rand.nextInt(50) + 1;
+      fonts.add(n);
+    }
+
+    Random num = new Random();
+    int myNum = num.nextInt(2) + 1;
+
     model.addAttribute("name", name);
     model.addAttribute("counter", id.incrementAndGet());
     model.addAttribute("hellos", hellos);
+    model.addAttribute("color", "color" + myNum);
     return "greeting";
   }
 }
