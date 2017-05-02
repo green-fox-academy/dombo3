4b6e3fd51ec05e8b30f1310c070880700a7ce826
Dombovari Peter <dombo.peter@gmail.com>
2017-05-02 12:22:20 +0200

    count greeting calls

--- week-08/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
+++ week-08/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
@@ -1,16 +1,19 @@
 package com.greenfoxacademy.controllers;
 
 import com.greenfoxacademy.Greeting;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
+  AtomicLong id = new AtomicLong();
 
   @RequestMapping(value = "/greeting")
-  public Greeting greeting(@RequestParam("name") String name) {
-    Greeting greet = new Greeting(1, "Hello " + name);
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "Hello World", required = false) String name) {
+    Greeting greet = new Greeting(id.incrementAndGet(), "Hello " + name);
     return greet;
   }
 }
