d2484ee49a88d952c7dd6cf3e6224fcb07cdba46
Dombovari Peter <dombo.peter@gmail.com>
2017-05-03 14:53:32 +0200

    use th:each on BankAccounts

--- week-08/LionKing/src/main/java/com/greenfoxacademy/controllers/BankAccountController.java
+++ week-08/LionKing/src/main/java/com/greenfoxacademy/controllers/BankAccountController.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.controllers;
 
 import com.greenfoxacademy.model.BankAccount;
 import com.greenfoxacademy.model.Result;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,18 @@ public class BankAccountController {
     BankAccount account = new BankAccount("Simba", 2000, "lion");
     model.addAttribute("bankaccount", account);
     return "exercise1";
+  }
+
+  @RequestMapping("/exercise5")
+  public String accountsArray(Model model) {
+    List<BankAccount> animalAccounts = new ArrayList<>();
+    animalAccounts.add(new BankAccount("Nala",1000, "lion"));
+    animalAccounts.add(new BankAccount("Zordon",500, "lion"));
+    animalAccounts.add(new BankAccount("Mufasa",200, "lion"));
+    animalAccounts.add(new BankAccount("Timon",900, "suricata"));
+    animalAccounts.add(new BankAccount("Pumba",2,"warthog"));
+    model.addAttribute("accounts", animalAccounts);
+    return "exercise5";
   }
 
   @GetMapping("/form")
