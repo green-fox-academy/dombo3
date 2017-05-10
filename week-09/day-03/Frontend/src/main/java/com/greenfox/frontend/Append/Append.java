7b568fb6ba1358976ada36d41247c772e4b8c0f8
Dombovari Peter <dombo.peter@gmail.com>
2017-05-10 17:02:50 +0200

    append 'a' to a pathvariable as response to a get request

---
+++ week-09/day-03/Frontend/src/main/java/com/greenfox/frontend/Append/Append.java
@@ -0,0 +1,13 @@
+package com.greenfox.frontend.Append;
+
+public class Append {
+  private String appended;
+
+  public Append(String appended) {
+    this.appended = appended;
+  }
+
+  public String getAppended() {
+    return appended;
+  }
+}
