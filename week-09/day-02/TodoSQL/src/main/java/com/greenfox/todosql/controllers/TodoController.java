b8480b669f4ac0ac3cd9601c8553bfe2bb7e0e39
Dombovari Peter <dombo.peter@gmail.com>
2017-05-11 21:56:29 +0200

    feat(add): handle requests for adding new todos to TodoList

--- week-09/day-02/TodoSQL/src/main/java/com/greenfox/todosql/controllers/TodoController.java
+++ week-09/day-02/TodoSQL/src/main/java/com/greenfox/todosql/controllers/TodoController.java
@@ -1,9 +1,13 @@
 package com.greenfox.todosql.controllers;
 
+import com.greenfox.todosql.model.Todo;
 import com.greenfox.todosql.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,5 +30,17 @@ public class TodoController {
       model.addAttribute("todos", todoRepo.findAll());
     }
     return "todo";
+  }
+
+  @GetMapping(value = "/add")
+  public String getAddForm(Model model) {
+    model.addAttribute("todo", new Todo());
+    return "add";
+  }
+
+  @PostMapping(value = "/add")
+  public String add(@ModelAttribute Todo todo) {
+    todoRepo.save(todo);
+    return "redirect:";
   }
 }
