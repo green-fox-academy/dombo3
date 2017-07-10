4734abdb1772b1ff79bfb96ca12979e29ecf3f29
Dombovari Peter <dombo.peter@gmail.com>
2017-07-10 09:39:45 +0200

    feat(ArrayChecker): add simplest solution for checking two arrays

--- FinalExam/src/ArrayChecker.java
+++ FinalExam/src/ArrayChecker.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class ArrayChecker {
 
   public boolean isSameArray() {
     return true;
   }
 
-  public boolean isSameArraySimple() {
-    return true;
+  public boolean isSameArraySimple(int[] a1, int[] a2) {
+    return Arrays.equals(a1,a2);
   }
 }
--- FinalExam/src/Main.java
+++ FinalExam/src/Main.java
@@ -2,7 +2,16 @@ public class Main {
 
   public static void main(String[] args) {
     ArrayChecker arrayChecker = new ArrayChecker();
+
+    int a1[] = new int[] {
+        0, 1, 2, 3
+    };
+
+    int a2[] = new int[] {
+      0, 1, 2, 4
+    };
+
     System.out.println(arrayChecker.isSameArray());
-    System.out.println(arrayChecker.isSameArraySimple());
+    System.out.println(arrayChecker.isSameArraySimple(a1, a2));
   }
 }
