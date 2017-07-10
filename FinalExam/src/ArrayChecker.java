49391d362a3ad1233bee75615948836a01270885
Dombovari Peter <dombo.peter@gmail.com>
2017-07-10 09:33:31 +0200

    feat(ArrayChecker): create boilerplate code;

---
+++ FinalExam/src/ArrayChecker.java
@@ -0,0 +1,10 @@
+public class ArrayChecker {
+
+  public boolean isSameArray() {
+    return true;
+  }
+
+  public boolean isSameArraySimple() {
+    return true;
+  }
+}
---
+++ FinalExam/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+  public static void main(String[] args) {
+    ArrayChecker arrayChecker = new ArrayChecker();
+    System.out.println(arrayChecker.isSameArray());
+    System.out.println(arrayChecker.isSameArraySimple());
+  }
+}
